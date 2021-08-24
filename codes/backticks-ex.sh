44607652fcb42b443dcbe426ac23270efd14c40b
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2021-08-24 08:05:37 -0400

    Being_Useful_Using_Bash.tex, codes/backticks-ex.sh : 1. Added content about command substitution / backticks

---
+++ codes/backticks-ex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author  : Ali Snedden
+# License : MIT
+# Purpose : Illustrate Loops
+# Refs    : TLDP - Command Subsition : https://tldp.org/LDP/abs/html/commandsub.html
+#          
+#         
+
+
+#### Simple command substitution ####
+DIRSIZE=`du -hs`
+echo "Size of current directory : ${DIRSIZE}"
+
+
+#### In a Loop - sort of silly ####
+for f in `ls codes`; do
+    echo "f = ${f}"
+done
+
