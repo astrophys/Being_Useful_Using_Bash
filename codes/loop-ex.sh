fcd1f87f18245fb97b205927f0b04e73e868a543
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2021-08-24 07:55:06 -0400

    Being_Useful_Using_Bash.tex, codes/loop-ex.sh : 1. Added content on for loops

---
+++ codes/loop-ex.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Author  : Ali Snedden
+# License : MIT
+# Purpose : Illustrate Loops
+# Refs    : TLDP - Loops : https://tldp.org/LDP/abs/html/loops1.html
+#          
+#         
+
+
+#### Simple Counting for loop####
+echo "Counting to 10..."
+for((i=0; i<=10; i++)); do 
+    echo "i = ${i}"
+done
+
+
+echo 
+echo
+echo "Looping over code files..."
+for f in codes/*; do
+    echo "f = ${f}"
+done
+
+echo 
+echo
