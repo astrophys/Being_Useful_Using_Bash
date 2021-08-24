e3cef16763703d476b27e9cfdf81702798d80ffc
Ali Snedden <Ali.Snedden@nationwidechildrens.org>
2021-08-24 07:47:27 -0400

    Being_Useful_Using_Bash.tex, codes/if-statement-ex.sh : 1. Added if and if/else statements

---
+++ codes/if-statement-ex.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author  : Ali Snedden
+# License : MIT
+# Purpose : Illustrate conditionals
+# Refs    : TLDP - if statements : https://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
+#          
+#         
+
+#### Simple if statement ####
+echo "Simple if statement"
+if [ -a codes/if-statement-ex.sh ]; then    ## Note [  ] delineates conditional
+    echo "codes/if-statement-ex.sh EXISTS"
+fi
+
+
+#### Simple if else statement ####
+echo
+echo
+echo "if / else statement"
+if [ -a codes/if-statement-ex.sh ]; then    ## Note [  ] delineates conditional
+    echo "codes/if-statement-ex.sh EXISTS"
+else
+    echo "codes/if-statement-ex.sh DNE"
+fi
